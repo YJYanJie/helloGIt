72918b123ad31537d6ed40f622f5f494cef3b0bb
YJYanJie <1312691968@qq.com>
2020-05-23 23:12:43 +0800

    使用gitee来测试

--- HelloGit/src/main/java/yj/git/HelloGit.java
+++ HelloGit/src/main/java/yj/git/HelloGit.java
@@ -13,7 +13,6 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("主干添加");
         System.out.println("在线编辑");
-
-
+        System.out.println("gitee测试");
     }
 }
