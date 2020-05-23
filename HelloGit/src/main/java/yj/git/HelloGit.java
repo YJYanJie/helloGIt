3b7d04eb27a29481ded529aa35d2485e2d9ddf6e
YJYanJie <1312691968@qq.com>
2020-05-23 22:21:24 +0800

    添加文件

---
+++ HelloGit/src/main/java/yj/git/HelloGit.java
@@ -0,0 +1,12 @@
+package yj.git;
+
+/**
+ * @author yj
+ * @Description 测试
+ * @create 2020-05-23  22:13
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}
